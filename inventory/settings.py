dc23453a8fecc9994c68323018c1ac423ac1768f
sakthimugupython <mugunthanpythononline@vetritechnologysolutions.in>
2025-12-01 16:32:35 +0530

    first commit

--- inventory/settings.py
+++ inventory/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-aze4z4z&_3du4wd4by6y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
